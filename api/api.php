b134f39b1f3426df687637a2b4fb2a38441d8cd5
Will <will@williamyoumans.com>
2020-04-25 21:10:24 -0400

    formatting

--- api/api.php
+++ api/api.php
@@ -1,61 +1,59 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
 
 $search = $_GET['query'];
-$results = array();
+$results = [];
 if ($search) {
-  $entity = ($_GET['entity']) ? $_GET['entity'] : 'tvSeason';
-  $country = ($_GET['country']) ? $_GET['country'] : 'us';
-  $width = 600;
-  $height = 600;
-  $warning = '';
+    $entity = ($_GET['entity']) ? $_GET['entity'] : 'tvSeason';
+    $country = ($_GET['country']) ? $_GET['country'] : 'us';
+    $width = 600;
+    $height = 600;
+    $warning = '';
 
-  $shortFilm = false;
-  if ($entity == 'shortFilm') {
-    $shortFilm = true;
-    $entity = 'movie';
-  }
-  $url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term='.urlencode($search).'&country='.$country.'&entity='.$entity;
-  if ($shortFilm) {
-    $url .= '&attribute=shortFilmTerm';
-    $entity = 'shortFilm';
-  }
-  if ($_GET['entity'] == 'id' || $_GET['entity'] == 'idAlbum') {
-    $url = 'https://itunes.apple.com/lookup?id='.urlencode($search).'&country='.$_GET['country'];
-  }
+    $shortFilm = false;
+    if ($entity == 'shortFilm') {
+        $shortFilm = true;
+        $entity = 'movie';
+    }
+    $url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=' . urlencode($search) . '&country=' . $country . '&entity=' . $entity;
+    if ($shortFilm) {
+        $url .= '&attribute=shortFilmTerm';
+        $entity = 'shortFilm';
+    }
+    if ($_GET['entity'] == 'id' || $_GET['entity'] == 'idAlbum') {
+        $url = 'https://itunes.apple.com/lookup?id=' . urlencode($search) . '&country=' . $_GET['country'];
+    }
 
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $json = curl_exec($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $json = curl_exec($ch);
 
-  if ($errno = curl_errno($ch)) {
-      echo json_encode(array('error' => "We were unable to connect to iTunes. Please try again shortly."));
-      exit;
-  }
+    if ($errno = curl_errno($ch)) {
+        echo json_encode(['error' => 'We were unable to connect to iTunes. Please try again shortly.']);
+        exit;
+    }
 
-  curl_close($ch);
+    curl_close($ch);
 
-  $obj = json_decode($json);
+    $obj = json_decode($json);
 
+    foreach ($obj->results as $result) {
+        $data = [];
+        $data['url'] = str_replace('100x100', '600x600', $result->artworkUrl100);
 
-  foreach ($obj->results as $result) {
+        $hires = str_replace('100x100bb', '100000x100000-999', $result->artworkUrl100);
+        $parts = parse_url($hires);
+        $hires = 'http://is5.mzstatic.com' . $parts['path'];
 
-    $data = array();
-    $data['url'] = str_replace('100x100', '600x600', $result->artworkUrl100);
+        $data['hires'] = $hires;
+        $data['title'] = ($entity == 'movie') ? $result->trackName : $result->collectionName;
 
-    $hires = str_replace('100x100bb', '100000x100000-999', $result->artworkUrl100);
-    $parts = parse_url($hires);
-    $hires = 'http://is5.mzstatic.com'.$parts['path'];
-
-    $data['hires'] = $hires;
-    $data['title'] = ($entity == 'movie') ? $result->trackName : $result->collectionName;
-
-    switch ($entity) {
+        switch ($entity) {
       case 'musicVideo':
-        $data['title'] = $result->trackName.' (by '.$result->artistName.')';
+        $data['title'] = $result->trackName . ' (by ' . $result->artistName . ')';
         $data['url'] = $hires;
         $width = 640;
         $height = 464;
@@ -71,21 +69,21 @@ if ($search) {
         $warning = '(may not work)';
         break;
       case 'ebook':
-        $data['title'] = $result->trackName.' (by '.$result->artistName.')';
+        $data['title'] = $result->trackName . ' (by ' . $result->artistName . ')';
         $width = 400;
         $warning = '(probably won\'t work)';
         break;
       case 'album':
       case 'idAlbum':
-        $data['title'] = $result->collectionName.' (by '.$result->artistName.')';
+        $data['title'] = $result->collectionName . ' (by ' . $result->artistName . ')';
         //$warning = '(probably won\'t work)';
         break;
       case 'audiobook':
-        $data['title'] = $result->collectionName.' (by '.$result->artistName.')';
+        $data['title'] = $result->collectionName . ' (by ' . $result->artistName . ')';
         $warning = '(probably won\'t work)';
         break;
       case 'podcast':
-        $data['title'] = $result->collectionName.' (by '.$result->artistName.')';
+        $data['title'] = $result->collectionName . ' (by ' . $result->artistName . ')';
         break;
       case 'software':
         $data['url'] = str_replace('512x512bb', '1024x1024bb', $result->artworkUrl512);
@@ -98,15 +96,13 @@ if ($search) {
         break;
     }
 
-    if ($data['title']) {
-      $data['width'] = $width;
-      $data['height'] = $height;
-      $data['warning'] = $warning;
-      $results[] = $data;
+        if ($data['title']) {
+            $data['width'] = $width;
+            $data['height'] = $height;
+            $data['warning'] = $warning;
+            $results[] = $data;
+        }
     }
-  }
 }
 
 echo json_encode($results);
-
-?>
